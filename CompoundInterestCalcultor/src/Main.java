72d7ff4c39c953478aad6d99a360edc496a7c6d0
Al Shahoriar <122755884+alshahriar78@users.noreply.github.com>
2025-03-18 12:36:45 +0600

    String

--- CompoundInterestCalcultor/src/Main.java
+++ CompoundInterestCalcultor/src/Main.java
@@ -1,8 +1,34 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
+
+      //  CI = P (1 + r/n)nt - P
+//        A	=	final amount
+//                P	=	initial principal balance
+//        r	=	interest rate
+//                n	=	number of times interest applied per time period
+//                t	=	number of time periods elapsed
+
         Scanner scanner = new Scanner(System.in);
-        int year = scanner.nextInt();
-        System.out.println("");
+
+
+        System.out.println("Enter The principal Balance: ");
+        double PrincipalBalance = scanner.nextDouble();
+
+        System.out.println("Enter The Interest Rate in (%): ");
+        double Rate = scanner.nextDouble()/100;
+
+        System.out.println("Enter # Times Compound Per Year ");
+        int TimeCompound = scanner.nextInt();
+
+        System.out.println("Enter #   Year ");
+        int Years = scanner.nextInt();
+
+        double Amount = PrincipalBalance *(Math.pow(1+Rate/TimeCompound,TimeCompound*Years));
+        System.out.println(Amount);
+
+
+
     }
 }
---
+++ string/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String Month = "January";
+        char result = Month.charAt(4);
+        int WordLength = Month.length();
+        System.out.println(result);
+        System.out.println(WordLength);
+
+
+    }
+}
