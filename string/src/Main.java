c6d28508e2e891b176aeb92e85c0da802538dc13
Al Shahoriar <122755884+Alshahriar78@users.noreply.github.com>
2025-03-19 12:13:09 +0600

    ternary Operator Update in String Package

--- string/src/Main.java
+++ string/src/Main.java
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -14,17 +15,26 @@ public class Main {
 //            System.out.printf("The %s Takes Spaces\n", Name);
 //        }
         //substring(start, end)
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Your Email:");
-        String email = scanner.nextLine();
-        if(email.contains("@")){
-            String userName = email.substring(0,email.indexOf("@"));
-            String domain = email.substring(email.indexOf("@")+1);
-            System.out.println(userName);
-            System.out.println(domain);
-        }else{
-            System.out.println("Email Must Contains @ ");
-        }
+          Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter Your Email:");
+//        String email = scanner.nextLine();
+//        if(email.contains("@")){
+//            String userName = email.substring(0,email.indexOf("@"));
+//            String domain = email.substring(email.indexOf("@")+1);
+//            System.out.println(userName);
+//            System.out.println(domain);
+//        }else{
+//            System.out.println("Email Must Contains @ ");
+//        }
+         // ternary Operator
+          int age;
+          System.out.print("Enter Your Age:");
+          age = scanner.nextInt();
+          String isOld = (age>=18) ? "Adult" : "Child";
+          System.out.printf("You are %s.\n",isOld);
+
+          String isEven = (age%2==0) ? "Even Number":"Odd Number";
+          System.out.printf("The Number Is %s.",isEven);
 
 
      scanner.close();
